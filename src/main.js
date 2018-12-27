c49431ecc6f2d7b763e26b17995ab1e3449440bb
myrid <360381262@qq.com>
2018-12-27 12:21:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import './cube-ui'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import './cube-ui';
+import FastClick from 'fastclick';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+FastClick.attach(document.body);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
